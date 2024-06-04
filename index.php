a8f2f20ab32456e95346342999ee728bea06239e
franck <franck.tanoh@manchester.ac.uk>
2024-06-04 15:34:50 +0100

    PT-585 update phpCAS to lates version 1.6.1. This also includes local changes

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ *   Example for a simple cas 2.0 client
+ *
+ * PHP Version 5
+ *
+ * @file     example_simple.php
+ * @category Authentication
+ * @package  PhpCAS
+ * @author   Joachim Fritschi <jfritschi@freenet.de>
+ * @author   Adam Franco <afranco@middlebury.edu>
+ * @license  http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ * @link     https://wiki.jasig.org/display/CASC/phpCAS
+ */
+
+include 'lib/conf/conf.php';
+// Load the settings from the central config file
+require_once 'config.php';
+// Load the CAS lib
+require_once 'CAS.php';
+// Enable debugging
+phpCAS::setDebug();
+// Enable verbose error messages. Disable in production!
+phpCAS::setVerbose(false);
+// Initialize phpCAS
+phpCAS::client(CAS_VERSION_2_0, $cas_host, $cas_port, $cas_context);
+// For production use set the CA certificate that is the issuer of the cert
+// on the CAS server and uncomment the line below
+// phpCAS::setCasServerCACert($cas_server_ca_cert_path);
+// For quick testing you can disable SSL validation of the CAS server.
+// THIS SETTING IS NOT RECOMMENDED FOR PRODUCTION.
+// VALIDATING THE CAS SERVER IS CRUCIAL TO THE SECURITY OF THE CAS PROTOCOL!
+phpCAS::setNoCasServerValidation();
+// force CAS authentication
+phpCAS::forceAuthentication();
+
+// at this step, the user has been authenticated by the CAS server
+// and the user's login name can be read with phpCAS::getUser().
+// logout if desired
+if (isset($_REQUEST['logout'])) {
+  phpCAS::logout();
+}
+// for this test, simply print that the authentication was successfull
+?>
+<html>
+  <head>
+    <title>phpCAS simple client</title>
+  </head>
+  <body>
+    <h1>Successfull Authentication! </h1>
+    <p>the user's login is <b><?php echo phpCAS::getUser(); ?></b>.</p>
+    <p>phpCAS version is <b><?php echo phpCAS::getVersion(); ?></b>.</p>
+
+    <h3>User Attributes</h3>
+    <pre>
+      <?php
+
+      function print_array($arr) {
+        print "<table>";
+
+        foreach ($arr as $key => $value) {
+          print "<tr><td> $key </td><td>";
+          if (is_array($value)) {
+            print_array($value);
+          } else {
+            print $value;
+          }
+          print "</td> </tr>";
+        }
+        print "</table>";
+      }
+
+      $atts = phpCAS::getAttributes();
+      print_r($atts);
+      ?>
+
+    <p><a href="?logout=">Logout</a></p>
+  </body>
+</html>
